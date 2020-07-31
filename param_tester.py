397db8237d56d1e2595348047279b940ab92f51c
erfan377-gpuVM <erfan377@gmail.com>
2020-07-31 04:46:52 +0000

    save summaries as json in param_tester

--- param_tester.py
+++ param_tester.py
@@ -1,17 +1,21 @@
 from train import train_unet
-learn_rate = [1, 0.1, 0.001, 0.0001, 0.005]
-epoch = 15
+import json
+
+
+learn_rate = [0.001]
+epoch = 10
 scales = [1, 0.7, 0.5, 0.3]
 batches = [1, 2, 4, 8, 16, 32]
-val = 20
+val = 11
 
 net_test = train_unet()
 best_model = {'score': 0, 'properties' : ''}
 best_model['score'] = 10
+list_results = {}
 for lr_rate in learn_rate:
     for scale in scales:
         for batch in batches:
-            output_path = f'checkoints_{lr_rate}_BS_{batch}_SCALE_{scale}/'
+            output_path = f'checkpoints_fill/checkoints_LR_{lr_rate}_BS_{batch}_SCALE_{scale}_E_{epoch}/'
             try:
                 val_score = net_test.train_net(
                               epochs=epoch,
@@ -20,9 +24,10 @@ for lr_rate in learn_rate:
                               img_scale=scale,
                               val_percent=val / 100, 
                               dir_checkpoint=output_path)
-                result_summary = f'model_{lr_rate}_BS_{batch}_SCALE_{scale}_DICE_{val_score}'
+                result_summary = f'model_LR_{lr_rate}_BS_{batch}_SCALE_{scale}_DICE_{val_score}_E_{epoch}'
                 print(result_summary)
-                if val_score > best_model.score:
+                list_results[result_summary] = val_score
+                if val_score > best_model['score']:
                     best_model['score'] = val_score
                     best_model['properties'] = result_summary
             except KeyboardInterrupt:
@@ -33,3 +38,5 @@ for lr_rate in learn_rate:
                 except SystemExit:
                     os._exit(0)
 print(best_model['properties'])
+with open('mask_filled_results.json', 'w') as fp:
+    json.dump(list_results, fp)
