0636c9947112a38876429964427fda80dc3a99f6
erfan-vm2 <erfan377@gmail.com>
2020-08-06 08:17:47 +0000

    added CRF layer

--- eval.py
+++ eval.py
@@ -3,9 +3,9 @@ import torch.nn.functional as F
 from tqdm import tqdm
 
 from dice_loss import dice_coeff
+from utils.crf import dense_crf
 
-
-def eval_net(net, loader, device):
+def eval_net(net, loader, device, crf=True):
     """Evaluation without the densecrf with the dice coefficient"""
     net.eval()
     mask_type = torch.float32 if net.n_classes == 1 else torch.long
@@ -17,10 +17,17 @@ def eval_net(net, loader, device):
             imgs, true_masks = batch['image'], batch['mask']
             imgs = imgs.to(device=device, dtype=torch.float32)
             true_masks = true_masks.to(device=device, dtype=mask_type)
-
+            
             with torch.no_grad():
                 mask_pred = net(imgs)
-
+                
+            if crf:
+                crf_output = np.zeros(output.shape)
+                for i, (image, prob_map) in enumerate(zip(imgs, mask_pred)):
+                    image = image.transpose(1, 2, 0)
+                    crf_output[i] = dense_crf(image, prob_map)
+                mask_pred = crf_output
+                
             if net.n_classes > 1:
                 tot += F.cross_entropy(mask_pred, true_masks).item()
             else:
